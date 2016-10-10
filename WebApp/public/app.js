31d8a3505e50c0a6435877f142daa403220ada5c
Joon <ychoi302@gatech.edu>
2016-10-10 11:12:12 -0400

    added signin functionality

--- WebApp/public/app.js
+++ WebApp/public/app.js
@@ -17,4 +17,53 @@
             this.object = $firebaseObject(ref);
         });
 
+    const txtEmail = document.getElementById('txtEmail');
+    const txtPassword = document.getElementById('txtPassword');
+    const btnLogin = document.getElementById('btnLogin');
+    const btnSignup = document.getElementById('btnSignup');
+    const btnLogout = document.getElementById('btnLogout');
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    console.log(email);
+    console.log(pass);
+
+    btnLogin.addEventListener('click', e => {
+        // get email and pass
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+
+        const promise = auth.signInWithEmailAndPassword(email, pass);
+
+        promise.catch(e => console.log(e.message));
+
+    });
+
+    btnSignup.addEventListener('click', e => {
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+
+        const promise = auth.createUserWithEmailAndPassword(email, pass);
+
+        promise.catch(e => console.log(e.message));
+
+
+    });
+
+    btnLogout.addEventListener('click', e => {
+        firebase.auth().signOut();
+    });
+
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+            console.log(firebaseUser);
+            btnLogout.classList.remove('hide');
+        } else {
+            console.log("not logged in");
+            btnLogout.classList.add('hide');
+        }
+    });
+
+
 }());
