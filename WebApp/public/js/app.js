fe3694c2102eddfbded4a650f673ed0eddf5de29
rohan katyal <rohan12086@iiitd.ac.in>
2016-10-24 23:02:55 -0400

    [WIP] Dashboard backend

--- WebApp/public/js/app.js
+++ WebApp/public/js/app.js
@@ -10,70 +10,88 @@
     firebase.initializeApp(config);
 
     angular
-        .module('ducktab', ['firebase'])
-        .controller("MyCtrl", function ($firebaseObject) {
-            const rootRef = firebase.database().ref().child("survey questions");
-            this.object = $firebaseObject(rootRef);
+        .module('ducktab', ['firebase', 'ngRoute'])
+        .controller("MyCtrl", ['$scope', '$firebaseObject', '$document', '$location', function ($scope, $firebaseObject, $document, $location) {
+            var txtEmail = $document[0].getElementById('txtEmail');
+            var txtPassword = $document[0].getElementById('txtPassword');
+            var btnLogin = $document[0].getElementById('btnLogin');
+            var btnSignup = $document[0].getElementById('btnSignup');
+            var formLogin = $document[0].getElementById('loginForm');
+            var datadump = $document[0].getElementById('message');
+            var email = txtEmail.value;
+            var pass = txtPassword.value;
+            console.log(email);
+            console.log(pass);
+
+            btnLogin.addEventListener('click', e => {
+                // get email and pass
+                const email = txtEmail.value;
+                const pass = txtPassword.value;
+                const auth = firebase.auth();
+                const promise = auth.signInWithEmailAndPassword(email, pass);
+                promise.catch(e => console.log(e.message));
+
+            });
+
+            btnSignup.addEventListener('click', e => {
+                const email = txtEmail.value;
+                const pass = txtPassword.value;
+                const auth = firebase.auth();
+
+                const promise = auth.createUserWithEmailAndPassword(email, pass);
+
+                promise.catch(e => console.log(e.message));
+
+            });
+
+            firebase.auth().onAuthStateChanged(firebaseUser => {
+                if (firebaseUser) {
+                    console.log(firebaseUser);
+                    $location.path( "/home" );
+                } else {
+                    console.log("not logged in");
+                }
+            });
+        }])
+        .controller("HomeCtrl", ['$scope', '$firebaseObject', '$document', '$location', '$q', function ($scope, $firebaseObject, $document, $location, $q) {
+          var rootRef = firebase.database().ref().child("survey questions");
+          $firebaseObject(rootRef).$bindTo($scope, "surveyQuestions");
+          var goToQuestionDetail = function(){
+            $location.path("/questionDetail");
+          };
+          $scope.openDetails = function(surveyQuestion){
+              localStorage.setItem("questionToDetail", surveyQuestion);
+              goToQuestionDetail();
+          }
+          var btnLogout = $document[0].getElementById('btnLogout');
+          btnLogout.addEventListener('click', e => {
+              firebase.auth().signOut();
+              $location.path("/");
+          });
+        }])
+        .controller("QuestionDetailCtrl", ['$scope', '$firebaseObject', '$document', '$location', function ($scope, $firebaseObject, $document, $location) {
+          $scope.question = localStorage.getItem("questionToDetail");;
+          var rootRef = firebase.database().ref().child("survey questions")
+          $firebaseObject(rootRef).$bindTo($scope, "questionAnswers");
+          $scope.backToQuestions = function(){
+            $location.path('/home');
+          }
+
+        }])
+        .config(function($routeProvider){
+          $routeProvider
+          .when("/", {
+              templateUrl : "./login.html",
+              controller: "MyCtrl"
+          })
+          .when('/home', {
+            templateUrl: "./home.html",
+            controller: "HomeCtrl"
+          })
+          .when('/questionDetail', {
+            templateUrl: "./questionDetail.html",
+            controller: "QuestionDetailCtrl"
+          })
         });
-
-    const txtEmail = document.getElementById('txtEmail');
-    const txtPassword = document.getElementById('txtPassword');
-    const btnLogin = document.getElementById('btnLogin');
-    const btnSignup = document.getElementById('btnSignup');
-    const btnLogout = document.getElementById('btnLogout');
-    const formLogin = document.getElementById('loginForm');
-    const datadump = document.getElementById('message');
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    console.log(email);
-    console.log(pass);
-
-    btnLogin.addEventListener('click', e => {
-        // get email and pass
-        const email = txtEmail.value;
-        const pass = txtPassword.value;
-        const auth = firebase.auth();
-
-        const promise = auth.signInWithEmailAndPassword(email, pass);
-
-        promise.catch(e => console.log(e.message));
-
-    });
-
-    btnSignup.addEventListener('click', e => {
-        const email = txtEmail.value;
-        const pass = txtPassword.value;
-        const auth = firebase.auth();
-
-        const promise = auth.createUserWithEmailAndPassword(email, pass);
-
-        promise.catch(e => console.log(e.message));
-
-
-    });
-
-    btnLogout.addEventListener('click', e => {
-        firebase.auth().signOut();
-    });
-
-    firebase.auth().onAuthStateChanged(firebaseUser => {
-        if (firebaseUser) {
-            console.log(firebaseUser);
-            btnLogout.classList.remove('hide');
-            formLogin.classList.add('hide');
-            datadump.classList.remove('hide');
-
-
-
-        } else {
-            console.log("not logged in");
-            btnLogout.classList.add('hide');
-            datadump.classList.add('hide');
-            formLogin.classList.remove('hide');
-
-
-        }
-    });
-
 
 }());
